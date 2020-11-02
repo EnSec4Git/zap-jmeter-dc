4b7a1cd825b07516f530326a59109049b08139f5
EnSec4Git <EnSec4Git@localhost>
2020-11-02 23:51:48 +0200

    Fine tuning OWASP ZAP not starting properly. Docker permissions issues

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+ls -la /opt/
+ls -la /opt/lpwd/
+
 zap.sh -daemon -port 8080 -host 127.0.0.1 -config api.disablekey=true 1>/opt/lpwd/zaplog.txt 2>/opt/lpwd/zaperr.txt &
 
 sleep 10
